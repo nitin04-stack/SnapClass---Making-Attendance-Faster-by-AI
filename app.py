7ff5068cabc4f0bfa16c0179769b31ec04c52a7b
Nitin Kumawat <nk3190933@gmail.com>
2026-06-26 17:06:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ def main():
     if "login_type" not in st.session_state:
         st.session_state["login_type"] = None
 
+    join_code = st.query_params.get("join-code")
+    if join_code:
+        if st.session_state.login_type != "student":
+            st.session_state.login_type = "student"
+            st.rerun()
+
     match st.session_state["login_type"]:
         case "teacher":
             teacher_screen()
@@ -21,14 +27,7 @@ def main():
         case None:
             home_screen()
 
-
-    join_code = st.query_params.get("join_code")
     if join_code:
-        if st.session_state.login_type != "student":
-            st.session_state.login_type = "student"
-            st.rerun()
         if st.session_state.get("is_logged_in") and st.session_state.get("user_role") == "student":
             auto_enroll_dialog(join_code)
-    
-# if __name__ == "__main__":
 main()
